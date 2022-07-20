5a2658ff53aa1144ef7372b709dfe67cfa01ed74
susanna k <susannakhub@gmail.com>
2022-07-20 09:03:16 -0700

    eaf

---
+++ quackcare!/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  quackcare!
+//
+//  Created by Scholar on 7/19/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var homePoints: UILabel!
+    var shopVC = ShopViewController()
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    func displayPoints() {
+        homePoints.text = "\(shopVC.points)"
+    }
+
+    displayPoints()
+    
+    }
+}
+
---
+++ quackcare!/ShopViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ShopViewController.swift
+//  quackcare!
+//
+//  Created by Scholar on 7/19/22.
+//
+
+import UIKit
+
+class ShopViewController: UIViewController {
+
+    @IBOutlet weak var shopPoints: UILabel!
+    var points = 0
+    
+    //pointsLabel.text = points
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    func displayPoints() {
+        shopPoints.text = "\(points)"
+    }
+        
+    displayPoints()
+
+        // Do any additional setup after loading the view.
+    }
+    // action every time it is clicked function is called and points are added and displayed
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
