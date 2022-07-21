4d39823280a3de4032f6e65d654179b984bd2f25
Darshini Bhasker <bhasker.darshini@gmail.com>
2022-07-21 15:28:17 -0700

    ww

---
+++ quackcare!/HatsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HatsViewController.swift
+//  quackcare!
+//
+//  Created by Scholar on 7/21/22.
+//
+
+import UIKit
+
+
+
+class HatsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var partyHat: UIButton!
+    
+    @IBOutlet weak var cowboyHat: UIButton!
+    
+    @IBOutlet weak var crownHat: UIButton!
+    
+    @IBOutlet weak var sunglass: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
